8d21214ec7401a090dcec0fdefdf11126b64682a
Kurhan Artem <kurhanartem@MacBook-Air-uzivatela-Kurhan.local>
2025-12-18 22:41:03 +0100

    Fixed mistake

--- app/main.py
+++ app/main.py
@@ -1,27 +1,30 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: int) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+    def __init__(self, distance_from_city_center: int, clean_power: int,
+                 average_rating: int, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car):
-        cost = car.comfort_class * (
-                    self.clean_power - car.clean_mark) / self.distance_from_city_center * self.average_rating
+    def calculate_washing_price(self, car) -> float:
+        cost = (car.comfort_class *
+                (self.clean_power - car.clean_mark)
+                / self.distance_from_city_center
+                * self.average_rating)
         return round(cost, 1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car) -> float:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def several_cars(self, cars):
+    def several_cars(self, cars) -> float:
         total_inner = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -31,7 +34,8 @@ class CarWashStation:
         return round(total_inner, 1)
 
     def rate_service(self, new_raiting):
-        new_average_rating = (self.average_rating * self.count_of_ratings) + new_raiting
+        new_average_rating = (self.average_rating
+                              * self.count_of_ratings) + new_raiting
         denominater = self.count_of_ratings + 1
         self.average_rating = (new_average_rating / denominater, 1)
         self.count_of_ratings += 1
